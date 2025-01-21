68c81cc9e7fe26d69065cd001250e7b7becee878
chiheb nouri <chihebnouri@chihebs-MacBook-Pro.local>
2025-01-21 16:40:34 +0100

    prosit 1

---
+++ src/ZooManagement.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ZooManagement {
+    public static void main(String[] args) {
+        int nbrCages=20;
+        String zooName="my zoo";
+        System.out.println(zooName+" comporte"+nbrCages+" cages");
+        Scanner input = new Scanner(System.in);
+        System.out.println("donner le nom du zoo");
+        zooName = input.nextLine();
+        System.out.println("donner le nombre du cages");
+        while(!input.hasNextInt()) {
+            System.out.println("donner un nombre du cages valide");
+
+            input.next();
+        }
+        nbrCages = input.nextInt();
+
+        System.out.println(zooName+" comporte "+nbrCages+" cages");
+        input.close();
+    }
+}
